395c41061119a7f1e3c41885c4feb39e28f2c5f0
Jeff Ripke <jripke74@gmail.com>
2017-07-31 16:28:27 -0700

    add oauth token

--- RestaurantReviews/YelpAccount.swift
+++ RestaurantReviews/YelpAccount.swift
@@ -7,13 +7,32 @@
 //
 
 import Foundation
+import Locksmith
 
 struct YelpAccount {
     let accessToken: String
     let expiration: TimeInterval
     let grantDate: Date
+    static let service = "Yelp"
 }
 
 extension YelpAccount {
-    
+    struct Keys {
+        static let token = "token"
+        static let expirationPeriod = "expirationPeriod"
+        static let grantDate = "grantDate"
+    }
+    func save() throws {
+        try Locksmith.saveData(data: [Keys.token: accessToken, Keys.expirationPeriod: expiration, Keys.grantDate: grantDate.timeIntervalSince1970], forUserAccount: YelpAccount.service)
+    }
+    static func loadFromKeychain() -> YelpAccount? {
+        guard let dictionary = Locksmith.loadDataForUserAccount(userAccount: YelpAccount.service),
+            let token = dictionary[Keys.token] as? String,
+            let expiration = dictionary[Keys.expirationPeriod] as? TimeInterval,
+            let grantDateValue = dictionary[Keys.expirationPeriod] as? TimeInterval else {
+                return nil
+        }
+        let grantDate = Date(timeIntervalSince1970: grantDateValue)
+        return YelpAccount(accessToken: token, expiration: expiration, grantDate: grantDate)
+    }
 }

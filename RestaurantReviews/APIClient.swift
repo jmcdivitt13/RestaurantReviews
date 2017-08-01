1e8c95c411868d1e8b86ff2241b7a3f10724f439
Jeff Ripke <jripke74@gmail.com>
2017-08-01 09:45:08 -0700

    add APIClient

---
+++ RestaurantReviews/APIClient.swift
@@ -0,0 +1,65 @@
+//
+//  APIClient.swift
+//  RestaurantReviews
+//
+//  Created by Jeff Ripke on 8/1/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import Foundation
+
+protocol APIClient {
+    var session: URLSession { get }
+}
+
+enum APIError: Error {
+    case requestFailed
+    case jsonConversionFailure
+    case invalidData
+    case responseUnsuccessful
+    case jsonParsingFailure
+    
+    var localizedDescription: String {
+        switch self {
+        case .requestFailed:
+            return "Request Failed"
+        case .invalidData:
+            return "Invalid Data"
+        case .responseUnsuccessful:
+            return "Response Unsuccessful"
+        case .jsonParsingFailure:
+            return "JSON Parsing Failure"
+        case .jsonConversionFailure:
+            return "JSON Conversion Failure"
+        }
+    }
+}
+
+extension APIClient {
+    typealias JSON = [String: AnyObject]
+    typealias JSONTaskCompletionHandler = (JSON?, APIError?) -> Void
+    
+    func jsonTask(with request: URLRequest, completionHandler completion: @escaping JSONTaskCompletionHandler) -> URLSessionDataTask {
+        let task = session.dataTask(with: request) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(nil, .requestFailed)
+                return
+            }
+            if httpResponse.statusCode == 200 {
+                if let data = data {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject]
+                        completion(json, nil)
+                    } catch {
+                        completion(nil, .jsonConversionFailure)
+                    }
+                } else {
+                    completion(nil, .invalidData)
+                }
+            } else {
+                completion(nil, .responseUnsuccessful)
+            }
+        }
+        return task
+    }
+}
